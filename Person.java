a08da57dd809df14cf876d8a8350c887d2a5df47
James Pickles <picklesj85@gmail.com>
2015-11-16 21:13:29 +0000

    question 1

---
+++ Person.java
@@ -0,0 +1,37 @@
+public class Person {
+	private String name;
+	private int age;
+	private Person next;
+	private Person previous;
+	
+	public Person(String name){
+		this.name = name;
+		this.age = age;
+		this.next = null;
+		this.previous = null;
+	}
+	
+	public String getName() {
+		return name;
+	}
+	
+	public int getAge() {
+		return age;
+	}
+	
+	public Person getNext(){
+		return next;
+	}
+	
+	public Person getPrevious() {
+		return previous;
+	}
+	
+	public void setNext(Person next) {
+		this.next = next;
+	}
+	
+	public void setPrevious(Person previous) {
+		this.previous = previous;
+	}
+}

a2ec4ed33a532670c131bd3e82b4ca73ed4084d3
BBK-PiJ-2015-85 <picklesj85@gmail.com>
2015-11-21 17:53:00 +0000

    ironed out pointer errors and tested thoroughly. added comments to make code clearer.

--- OldPersonSupermarket.java
+++ OldPersonSupermarket.java
@@ -15,34 +15,43 @@ public class OldPersonSupermarket implements PersonQueue {
 	*/
 	public Person retrieve(){
 		if (head == null) {
+			//no one in the queue
 			System.out.println("There are no customers to serve.");
 			return null;			
+		} else if (head.getNext()  == null) {
+			//only one person in the queue
+			Person current = head;
+			System.out.println("Customer " + current.getName() + " has been served.");
+			head = null;
+			return current;			
 		} else {
 			Person current = head;
 			int oldest = 0;
-			while (current.getNext() != null) {
+			//go through queue and store the oldest age
+			while (current != null) {
 				if (current.getAge() > oldest) {
 					oldest = current.getAge();
 				}
 				current = current.getNext();
-			}			
+			}
+			current = head;
+			//go through queue until you reach the oldest and serve them
 			while (current.getAge() != oldest) {
-				current = current.getPrevious();
+				current = current.getNext();
 			}			
 			if (current.getPrevious() != null && current.getNext() != null) {
+				//oldest has customers either side of him in the queue
 				current.getPrevious().setNext(current.getNext());
 				current.getNext().setPrevious(current.getPrevious());
 				System.out.println("Customer " + current.getName() + " has been served.");
 				return current;				
-			} else if (current.getPrevious() == null && current.getNext() == null) {
-				head = null;
-				System.out.println("Customer " + current.getName() + " has been served.");
-				return current;				
 			} else if (current.getPrevious() == null) {
+				//oldest is last in the queue
 				head = current.getNext();
 				System.out.println("Customer " + current.getName() + " has been served.");
 				return current;				
 			} else {
+				//oldest is first in the queue
 				current.getPrevious().setNext(null);
 				System.out.println("Customer " + current.getName() + " has been served.");
 				return current;				
@@ -69,39 +78,31 @@ public class OldPersonSupermarket implements PersonQueue {
 	}
 	
 	public void launch() {
-		Person p1 = new Person("Dave", 5);
+		Person p1 = new Person("Dave", 55);
 		addPerson(p1);
 		Person p2 = new Person("Tim", 10);
 		addPerson(p2);	
-		servePerson();
-		servePerson();
-
-		/**
-		Person p3 = new Person("Ben", 20);
+		Person p3 = new Person("Ben", 78);
 		addPerson(p3);
 		Person p4 = new Person("Sam", 30);
 		addPerson(p4);
-		System.out.println(queueLength());
-		servePerson();
-		System.out.println(queueLength());
-		Person p5 = new Person("Jim", 55);
-		addPerson(p5);
-		servePerson();
-		System.out.println(queueLength());
+		Person p5 = new Person("Jim", 24);
+		addPerson(p5);		
 		Person p6 = new Person("Dan", 75);
 		addPerson(p6);
-		servePerson();
-		System.out.println(queueLength());
-		Person p7 = new Person("Will", 80);
+		Person p7 = new Person("Will", 1);
 		addPerson(p7);
-		servePerson();
 		System.out.println(queueLength());
 		servePerson();
 		servePerson();
 		servePerson();
+		servePerson();
+		servePerson();
+		servePerson();
+		servePerson();
 		System.out.println(queueLength());
 		servePerson();
-		**/
+		
 
 	}
 	public static void main(String[] args) {
